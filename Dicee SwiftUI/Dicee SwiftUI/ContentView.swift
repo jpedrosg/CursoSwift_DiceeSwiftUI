43ca777cc44c03b66cffcbb66df44635f3d44c7c
João Pedro Giarrante <joao.giarrante@valemobi.com.br>
2020-09-25 19:32:05 -0300

    finishing project

--- Dicee SwiftUI/Dicee SwiftUI/ContentView.swift
+++ Dicee SwiftUI/Dicee SwiftUI/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack{
+            Image("background")
+                .resizable()
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer() // Espaços: máximo possível
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer() // Espaços: máximo possível
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }, label: {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                })
+                .background(Color.red)
+            }
             .padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100, height: 100, alignment: .center)
     }
 }
